396c76ea51152fe0eabca4599a2d48f261497ed3
Michael Richardson <michaelgocougs@gmail.com>
2018-07-26 18:21:03 -0700

    faster animation

--- src/app/animations/stagger.ts
+++ src/app/animations/stagger.ts
@@ -6,10 +6,10 @@ export const staggerItems =
             query('.stagger-from-top', style({ opacity: 0, transform: 'translateY(-40px)' }), {optional: true}),
             query('.stagger-from-bottom', style({ opacity: 0, transform: 'translateY(40px)' }), {optional: true}),
             query('.stagger-from-top, .stagger-from-bottom', stagger('50ms', [
-                animate('800ms 800ms ease', style({ opacity: 1, transform: 'translateY(0)' })),
+                animate('400ms 400ms ease', style({ opacity: 1, transform: 'translateY(0)' })),
             ])),
             query('.stagger-from-top, .stagger-from-bottom', [
-                animate(800, style('*'))
+                animate(400, style('*'))
             ])
         ])
     ])
