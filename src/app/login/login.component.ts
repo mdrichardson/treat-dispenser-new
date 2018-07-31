da0ee57e035bee26d41edf97deeefcd5b6bd6023
Michael Richardson <michaelgocougs@gmail.com>
2018-07-31 11:22:18 -0700

    stores JWT in localStorage

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -26,7 +26,10 @@ export class LoginComponent implements OnInit {
     loginUser() {
         this.db.login(this.user)
         .subscribe(
-            res => console.log(res),
+            res => {
+                console.log(res);
+                localStorage.setItem('token', res.token);
+            },
             err => console.log(err)
         )
     }
