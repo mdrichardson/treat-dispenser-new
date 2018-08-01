3773738aadf5eabb6c6dd0d4b73975821f9ab1b6
Michael Richardson <michaelgocougs@gmail.com>
2018-07-31 19:24:35 -0700

    now that db is working, removed from .gitignore

---
+++ src/app/database.service.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { BehaviorSubject } from 'rxjs';
+
+export interface userInterface {
+    username: string;
+    role: string;
+	photonDeviceId: string;
+	photonAccessToken: string;
+	videoUrl: string;
+	videoAuthToken: string;
+	photonApiUrl: string;
+	photonAccessString: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class DatabaseService {
+
+    private loginUrl = 'http://localhost:3000/api/login';
+    public user: userInterface;
+    private token: string;
+    private userLoggedInSource = new BehaviorSubject(false);
+    public userLoggedIn = this.userLoggedInSource.asObservable();
+
+    constructor(private http: HttpClient, public jwt: JwtHelperService) {
+    }
+    
+    // Log user in via Mongoose API
+    login = (user) => this.http.post<any>(this.loginUrl, user);
+
+    // Verify that user is logged in AND that their JWT is valid
+    loggedIn = () => {
+        const token: string = this.jwt.tokenGetter()
+        if (!token) {
+            this.userLoggedInSource.next(false);
+            return false
+        }
+        const tokenExpired: boolean = this.jwt.isTokenExpired(token)
+        if (!tokenExpired) {
+            this.getUser();
+            this.userLoggedInSource.next(true);
+        }
+        return !tokenExpired
+    }
+
+    getToken(): string {
+        if (!this.token) {
+          this.token = localStorage.getItem('token');
+        }
+        return this.token;
+      }
+
+    getUser = <userInterface>() => {
+        const token = this.getToken();
+        if (token) {
+            let decoded = this.jwt.decodeToken(token);
+            this.user = {
+                username: decoded.username,
+                role: decoded.role,
+                photonDeviceId: decoded.photonDeviceId,
+                photonAccessToken: decoded.photonAccessToken,
+                videoUrl: decoded.videoUrl,
+                videoAuthToken: decoded.videoAuthToken,
+                photonApiUrl: decoded.photonApiUrl,
+                photonAccessString: decoded.photonAccessString,
+            }
+            return this.user
+        }
+        return null
+    }
+}
