73d5c9a37f3de069e8c7bb03bd045fba31c3102c
Michael Richardson <michaelgocougs@gmail.com>
2018-08-02 14:35:27 -0700

    removed sourceMapping to prevent chrome dev whining

--- fix-sw.sh
+++ fix-sw.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 sed -i "/onFetch(event) {/,/const req = event.request;/c onFetch(event){const req=event.request;const whitelist=['particle','api','3000'];if(whitelist.some(word=>req.url.toLowerCase().includes(word.toLowerCase()))){return}" ./dist/treat-dispenser/ngsw-worker.js
+echo Added whitelist to ngsw-worker.js
+sed -i -e "/\/\/# sourceMappingURL=ngsw_worker.es6.js.map/d" ./dist/treat-dispenser/ngsw-worker.js
+echo Removed sourceMapping from ngsw-worker.js
